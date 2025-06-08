1f711e4ac6bf6a9b734ad9b5e786f13a4897b235
shevass02 <shevass02@gmail.com>
2025-06-08 13:49:58 +0300

    Домашнее задание по Python от 04.06

---
+++ Home.py
@@ -0,0 +1,24 @@
+# print('To be\nor not\nto be')
+# print("\"Life is what happens \n   when \n   you're busy making other plans\" \n   John Lennon")
+# print("\"The best way to predict the future\n   is to\n      invent it\"\n         Alan Kay")
+
+# a = int(input("Діагональ №1 - "))
+# b = int(input("Діагональ №2 - "))
+# print((a*b)/2)
+
+# a = int(input("Зарплата за місяць,грн - "))
+# b = int(input("Місячний платіж за кредит, грн - "))
+# c = int(input("Комунальні послуги, грн - "))
+# print(a-(b+c))
+
+# a = float(input("Сума рахунку в ресторані,грн - "))
+# b = float(input("Кількість осіб - "))
+# print((15*a)/100)
+# print((15*a)/100+a)
+# print(((15*a)/100+a)/b)
+
+# a = float(input("Вартість оренди авто за день, грн - "))
+# b = int(input("Кількість днів оренди - "))
+# c = int(input("Сума застави, грн - "))
+# print(b*a-c)
+# print((b*a-c)/b)
---
+++ index.py
@@ -0,0 +1,102 @@
+# # print(3,5,7, sep=" hi ")
+# # print("Hello World",3,4,5,6, sep=",end="/n)
+# """
+# ghfhjdg
+# """
+
+
+# age = 20 # int
+# price = 45.7 # float
+# name = "Ivan" 
+# isSuccess = True #
+
+# print("вік:", age)
+# print("Ціна:", price)
+
+
+# a = 25
+# a = a-5
+# print(a+10)
+# print(a)
+
+# print(2**3)
+# print(100**2)
+
+
+# t=input("")
+
+# print("Nothing")
+# print("will work")
+# print("unless you do")
+
+# print('"Anyone who\n stops \n learning is old,\n whether at twenty or eighty \n He
+
+# print('"Life is what happens\n  when\n  you're busy making other plans"\n   John Lennon')
+# 
+# jhgjjhfhgf
+# dfsdf
+# sdfsd
+# sdfsdf
+# print(25**0.5)
+# print(10%7)
+# t=int(input("Введіть число: "))
+
+# print(int(t)*2)
+# print("\033[31mdgfhydgtr\033[0m\ndhfgyshcbhf\ndjfhcyc bf")
+# print("\"Life is what happens \n when \n you're busy making other plans\" \n John Lennon")
+
+# price = 3.4
+# print(type(False))
+# quantity = int(input("Количество продуктов на складе - "))
+# price = float(input("Цена продукта - "))
+# weight = float(input("Вес продукта"))
+
+# print("Количество продуктов на складе", quantity)
+# print("Цена продукта", price)
+# print("Вес продукта", weight)
+
+# asd = int(input("Сколько тебе лет? - "))
+# azs = int(input(" ррп"))
+# bbi = int(input(" iij"))
+
+# print("Сколько тебе лет? - ",asd)
+# print(" ррп",azs)
+# print("iij",bbi)
+# print( ((3>2) and (4<7)))
+
+# a = int(input("N1"))
+# b = int(input("N2"))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+# a = int(input("первое число"))
+# b = int(input("второе чило"))
+# print((a*b)/100)
+
+# _import = 5
+# print(5>3)
+# print(5!=5 or 5>4)
+
+# productName = input("Enter name: ")
+# productPrice = float(input("Enter price: "))
+# productCount = int(input("Enter count: "))
+
+# # Загальний результат по продукту Ім'яПродукта за кількість Кількість
+# #=result
+# #інтерполяція
+# print(f"Загальний результат по продукту {productName} за кількість "
+#       f"{productCount} = {productCount*productPrice}")
+
+# metr = (int(input("метр:")))
+# cm = metr*100
+# mm = metr*1000
+# dm = metr*10
+# print(f"см:{cm}")
+# print(f"mm{mm}")
+# print(f"дм:{dm}")
+
+C = float(input("Температура по Цельсію"))
+print((C*(9/5))+32)
+
+
