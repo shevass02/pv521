f5f2e88707cf29dd4c3658a37ac3555a505bd5dd
shevass02 <shevass02@gmail.com>
2025-06-10 21:46:25 +0300

    Дом.Задание

--- lesson09.06.py
+++ lesson09.06.py
@@ -1,1 +1,58 @@
-number = int(input)
+# X= None
+# X = int(input("Enter x"))
+# if X>0:
+#     X=X+5
+# else:
+#     X=X*3
+# print(X)
+
+
+# t = int(input("Enter t: "))
+# if t<0:
+#     print("Треба одягти куртку")
+# elif t>=0 and t<15:
+#     print("Треба легку куртку")
+# else:
+#     print("Можна без куртки")
+
+
+# color = input("Enter color - ")
+# if color=="green":
+#     print("Йди")
+# elif 
+
+# Products = int(input("Введіть кількість товарів - "))
+# if Products>5:
+#     print("sale 20%")
+# elif Products==3 or Products==4:
+#     print("знижка 10%")
+# else:
+#     print("без знижки")
+
+# Ball = int(input("Введіть бал - "))
+# if Ball>=90:
+#     print("A")
+# elif Ball>=75 and Ball<=89:
+#     print("B")
+# if Ball<=60:
+#     print("F")
+
+# Oclock = int(input("Введіть час - "))
+# if Oclock>=6 and Oclock<=11:
+#     print("Доброго ранку")
+# elif Oclock>=12 and Oclock<=17:
+#     print("Добрий день")
+# elif Oclock>=18 and Oclock<=21:
+#     print("Добрий вечір")
+# else:
+#     print("Надобраніч")
+
+# Drink = input("Введіть напій - ")
+# if Drink == "tea":
+#     print("Ви обрали:чай")
+# if Drink == "coffee":
+#     print("Ваш напій:кава ")
+# if Drink == "Cjk":
+#     print("Ваш напій: сік")
+
+
